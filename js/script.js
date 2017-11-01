0a360bc4616550a86c27b2fba33d9889b5941d52
maki3p <josifovski.marjan@hotmail.com>
2017-11-01 10:05:54 +0100

    set nav bar on scroll

--- js/script.js
+++ js/script.js
@@ -1,48 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var year = new Date().getFullYear()
     $("#date").append(year)
-    console.log("Marjan Josifovski",year)
-$(".buton")
-            .hover(function() {
-            
-            $(this).css("color"," #575d66")
-  })
-            .mouseout(function() {
-                $(this).css("color","black")
-  });
+    console.log("Marjan Josifovski", year)
+    $(".buton")
+        .hover(function () {
+
+            $(this).css("color", " #575d66")
+        })
+        .mouseout(function () {
+            $(this).css("color", "black")
+        });
+
+
+    var loadSkilsTarget = $("#works").offset().top;
+    var interval = setInterval(function () {
+        if ($(window).scrollTop() >= loadSkilsTarget) {
+            loadSkills1();
+            loadSkills2();
+            loadSkills3();
+            loadSkills4();
+            loadSkills5();
+            loadSkills6();
+            loadSkills7();
+            loadSkills8();
+            loadSkills9();
+            clearInterval(interval);
+        }
+    }, 250)
+
+    //   var navTarget = $("#navBar").offset().top;
+    //   var interval1 = setInterval(function() {
+    //       if ($(window).scrollTop() >= navTarget) {
+    //         $('#navBar').addClass('navbar-fixed');
+    //           clearInterval(interval1);
+    //       }else{
+    //         $('#navBar').removeClass('navbar-fixed');
+    //       }
+    //   }, 0)
+    
+    var navpos = $(window).height()
+    console.log(navpos.top);
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > navpos) {
+            $('#navBar').addClass('navbar-fixed');
+        } else {
+            $('#navBar').removeClass('navbar-fixed');
+        }
+    });
    
 
-  var loadSkilsTarget = $("#works").offset().top;
-  var interval = setInterval(function() {
-      if ($(window).scrollTop() >= loadSkilsTarget) {
-        loadSkills1();
-        loadSkills2();
-        loadSkills3();
-        loadSkills4();
-        loadSkills5();
-        loadSkills6();
-        loadSkills7();
-        loadSkills8();
-        loadSkills9();
-          clearInterval(interval);
-      }
-  }, 250)
 
-  var navTarget = $("#navBar").offset().top;
-  var interval1 = setInterval(function() {
-      if ($(window).scrollTop() >= navTarget) {
-        $('#navBar').addClass('navbar-fixed');
-          clearInterval(interval1);
-      }
-  }, 0)
+    $("#mdbm").on('click', function () {
+        var url = 'https://github.com/maki3p/MDBM-frontend';
+        window.open(url, '_blank')
 
-  
+    });
 
-          $("#mdbm").on('click', function(){
-            var url = 'https://github.com/maki3p/MDBM-frontend';
-            window.open(url, '_blank')
-            
-        });
-       
-          
+
 })
